9c445f5a0e571eebc9fa61fe21e5a65a35efeb9f
alexjtark <alex.tarkowski@gmail.com>
2015-01-28 16:36:22 -0500

    add first ajax call to login link

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
 
+ $('header').on('click', '#login-link', function(event) {
+     event.preventDefault();
+    var $target = $(event.target);
+
+    $.ajax({
+      url: '/login',
+      type: 'GET'
+    }).done(function(response) {
+      $('#hidden-login').html(response);
+    });
+  });
 });
+
