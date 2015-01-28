040b9d04b4a53cf8101d883b1f02207fd6e4b2f5
alexjtark <alex.tarkowski@gmail.com>
2015-01-28 11:21:11 -0500

    fix routing for main login and signup

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -12,12 +12,12 @@ get '/signout' do
 end
 
 post '/login' do
- user = User.find_by(user_name: params[:user][:user_name])
+ user = User.find_by(username: params[:user][:username])
   if user.try(:authenticate, params[:user][:password])
     session[:user_id] = user.id
-    redirect '/'
+    erb :'/main'
   else
-    redirect '/'
+    erb :'/auth/_login'
   end
 end
 
