871de50c6e6c8239dd67f3196b461de3eeeaaa6c
alexjtark <alex.tarkowski@gmail.com>
2015-01-28 11:40:28 -0500

    add post views to box

--- app/controllers/box.rb
+++ app/controllers/box.rb
@@ -11,3 +11,12 @@ get '/box/:id' do |id|
   erb :'/box/show'
 end
 
+post '/box/new' do
+  box = Box.new(params[:box])
+  box[:user_id] = current_user.id
+  if box.save
+    erb :main
+  else
+    redirect '/box/new'
+  end
+end
