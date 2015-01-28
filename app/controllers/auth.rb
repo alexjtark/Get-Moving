4bce8b3b90099b115b2f0f19b9ec8fd12df39eef
alexjtark <alex.tarkowski@gmail.com>
2015-01-28 10:48:00 -0500

    add user login and signup post routes

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -3,7 +3,31 @@ get '/login' do
 end
 
 get '/signup' do
+  erb :'/auth/_signup'
+end
+
+get '/signout' do
   session.clear
   redirect '/'
+end
+
+post '/login' do
+ user = User.find_by(user_name: params[:user][:user_name])
+  if user.try(:authenticate, params[:user][:password])
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    redirect '/'
+  end
+end
+
+post '/signup' do
+  user = User.new(params[:user])
+  if user.save
+    session[:user_id] = user.id
+    redirect "/"
+  else
+    redirect "/"
+  end
 end
 
