ef20bdfbd9fb836041265842a57abb431942d7ff
alexjtark <alex.tarkowski@gmail.com>
2015-01-28 17:06:58 -0500

    add jquery styling on a hrefs

--- public/js/application.js
+++ public/js/application.js
@@ -11,5 +11,11 @@ $(document).ready(function() {
       $('#hidden-login').html(response);
     });
   });
-});
 
+$('a').mouseenter(function(){
+  $(this).css('background-color', 'yellow');
+});
+$('a').mouseleave(function(){
+  $(this).css('background-color', 'white');
+});
+});
